c369a2834ed3b22cccf2a821a8be0d5c763cd0df
heathergibb <hrgibb@gmail.com>
2024-02-28 20:43:10 -0700

    Completed core requirements for Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter a grade percentage: ");
+        string gradeStr = Console.ReadLine();
+
+        int grade = 0;
+        bool result = int.TryParse(gradeStr, out grade);
+
+        // if the use entered an interger, else give error message
+        if (result)
+        {
+            string letter = "";
+
+            if (grade >= 90)
+            {   
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            string strMessage = "";
+
+            if (grade >= 70)
+            {
+                strMessage = "Congratulations! You passed the course.";
+            }
+            else
+            {
+                strMessage = "Sorry, that is not a passing grade. Please try again.";
+            }
+
+            Console.WriteLine($"Your grade letter is {letter}. {strMessage}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid entry. You must enter an integer.");
+        }
     }
 }
